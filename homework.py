c0ee7e2459fe44507f5ca0937f9e8f6024063534
dimasick11 <dimapyrin@gmail.com>
2020-05-21 12:58:15 +0300

    one

--- homework.py
+++ homework.py
@@ -1,7 +1,8 @@
 import os
+import time
+
 import requests
 import telegram
-import time
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,8 +14,8 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    if homework['status'] == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -22,14 +23,16 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    params = {'from_date': current_timestamp}
+    homework_statuses = requests.get('https://praktikum.yandex.ru/api/user_api/homework_statuses/', headers=headers, params=params)
     return homework_statuses.json()
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    proxy = telegram.utils.request.Request(proxy_url='http://91.82.85.154:3128')
+    bot=telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
+    return bot.send_message(CHAT_ID, message)
 
 
 def main():
@@ -38,6 +41,7 @@ def main():
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
+            print(new_homework)
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(new_homework.get('homeworks')[0]))
             current_timestamp = new_homework.get('current_date')  # обновить timestamp
